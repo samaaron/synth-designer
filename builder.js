e86da4e8a503c05cacc5316639ec14cd9aa18a98
guyjbrown <g.j.brown@sheffield.ac.uk>
2023-10-26 23:40:01 +0100

    shunting yard complete

--- builder.js
+++ builder.js
@@ -61,6 +61,78 @@ function makeGrammar() {
 
   // make the grammar
   synthGrammar = ohm.grammar(source);
+  semantics = synthGrammar.createSemantics();
+
+  semantics.addOperation("interpret", {
+    Graph(a) {
+      return `[${"".concat(a.children.map(z => z.interpret()))}]`;
+    },
+    Tweak(a, b, c) {
+      return `{"tweak":{${a.interpret()},${c.interpret()}}}`;
+    },
+    comment(a, b) {
+      return `{"comment":"${b.sourceString.trim()}"}`;
+    },
+    tweakable(a, b, c) {
+      return `"id":"${a.sourceString}", "param":"${c.sourceString}"`;
+    },
+    Exp(a) {
+      return `"expression":"${a.interpret()}"`;
+    },
+    AddExp(a) {
+      return a.interpret();
+    },
+    AddExp_add(a, b, c) {
+      return `${a.interpret()}+${c.interpret()}`;
+    },
+    AddExp_subtract(a, b, c) {
+      return `${a.interpret()}-${c.interpret()}`;
+    },
+    MulExp(a) {
+      return a.interpret();
+    },
+    MulExp_times(a, b, c) {
+      return `${a.interpret()}*${c.interpret()}`
+    },
+    MulExp_divide(a, b, c) {
+      return `${a.interpret()}/${c.interpret()}`
+    },
+    ExpExp_paren(a, b, c) {
+      return `(${b.interpret()})`;
+    },
+    ExpExp_neg(a, b) {
+      return `-${b.interpret()}`;
+    },
+    ExpExp(a) {
+      return a.interpret();
+    },
+    Function_map(a, b, c, d, e, f, g, h) {
+      return `map(${c.interpret()},${e.interpret()},${g.interpret()})`;
+    },
+    Function_random(a, b, c, d, e, f) {
+      return `random(${c.interpret()},${e.interpret()})`
+    },
+    Function_exp(a, b, c, d) {
+      return `exp(${c.interpret()})`
+    },
+    Function_log(a, b, c, d) {
+      return `log(${c.interpret()})`
+    },
+    number(a) {
+      return a.interpret();
+    },
+    integer(a, b) {
+      const sign = (a.sourceString == "-") ? -1 : 1;
+      return sign * parseInt(b.sourceString);
+    },
+    floatingpoint(a, b, c, d) {
+      const sign = (a.sourceString == "-") ? -1 : 1;
+      return sign * parseFloat(b.sourceString + "." + d.sourceString);
+    },
+    control(a, b, c) {
+      return `param.${c.sourceString}`;
+    }
+  });
 
 }
 
@@ -68,7 +140,73 @@ function makeGrammar() {
 // helper function to get the grammar source, written in Ohm
 // ------------------------------------------------------------
 
+function getGrammarSource() {
+  return String.raw`
+  Synth {
 
+  Graph = Statement+
+
+  Statement = comment 
+  | Tweak 
+
+  comment (a comment)
+  = "#" commentchar* 
+
+  commentchar = alnum | "." | "+" | "-" | "/" | "*" | "." | blank
+
+  Tweak = tweakable "=" Exp 
+
+  Exp 
+    = AddExp
+
+  AddExp 
+    = AddExp "+" MulExp  -- add
+  | AddExp "-" MulExp  -- subtract
+  | MulExp 
+
+  MulExp 
+    = MulExp "*" ExpExp -- times
+    | MulExp "/" ExpExp -- divide
+    | ExpExp
+
+  ExpExp 
+    = "(" AddExp ")" -- paren
+    | "-" ExpExp -- neg
+    | Function 
+    | number
+    | control
+
+  Function
+    = "map" "(" AddExp "," number "," number ")" -- map
+    | "random" "(" number "," number ")" -- random
+    | "exp" "(" AddExp ")" -- exp
+    | "log" "(" AddExp ")" -- log
+
+  control (a control parameter)
+  = "param" "." letter+
+
+  tweakable
+  = varname "." parameter
+
+  parameter = "pitch" | "detune" | "cutoff" | "resonance" | "attack" | "decay" | "sustain" | "release"
+
+  varname (a module name)
+  = lower alnum*
+    
+  number (a number)
+  = floatingpoint | integer
+
+  floatingpoint = "-"? digit+ "." digit+
+
+  integer = "-"? digit+
+
+  blank = " "
+
+}
+`;
+}
+
+/*
 function getGrammarSource() {
   return String.raw`
   Synth {
@@ -150,6 +288,7 @@ function getGrammarSource() {
 }
 `;
 }
+*/
 
 // ------------------------------------------------------------
 // parse the graph
@@ -158,11 +297,91 @@ function getGrammarSource() {
 function parseSynthSpec() {
 
   let result = synthGrammar.match(gui("synth-spec").value + "\n");
-  if (result.succeeded())
+  if (result.succeeded()) {
     gui("parse-errors").value = "OK";
-  else
+    const adapter = semantics(result);
+    const json = adapter.interpret();
+    gui("parse-errors").value = json;
+    parseExpressions(json);
+  } else
     gui("parse-errors").value = result.message;
 
+}
+
+function parseExpressions(json) {
+  const obj = JSON.parse(json);
+  for (const e of obj) {
+    if (e.tweak) {
+      console.log(e.tweak.expression);
+      convertToPostfix(e.tweak.expression);
+    }
+  }
+}
+
+function convertToPostfix(expression) {
+  // shunting yard algorithm with functions
+  const tokens = expression.split(/([\*\+\-\/\,\(\)])/g).filter(x => x);
+  const ops = { "+": 1, "-": 1, "*": 2, "/": 2 };
+  const funcs = { "log": 1, "exp": 1, "random": 1, "map": 1 };
+  let top = (s) => s[s.length - 1];
+  let stack = [];
+  let result = [];
+  for (let t of tokens) {
+    if (isNumber(t) || isIdentifier(t)) {
+      result.push(t);
+      continue;
+    }
+    if (t == "(") {
+      stack.push(t);
+      continue;
+    }
+    if (t == ")") {
+      while (top(stack) != "(") {
+        let current = stack.pop();
+        result.push(current);
+      }
+      stack.pop();
+      if (stack.length > 0) {
+        if (top(stack) in funcs) {
+          let current = stack.pop();
+          result.push(current);
+        }
+      }
+      continue;
+    }
+    if (t in funcs) {
+      stack.push(t);
+      continue;
+    }
+    if (t == ",") {
+      while (top(stack) != "(") {
+        let current = stack.pop();
+        result.push(current);
+      }
+    }
+    if (t in ops) {
+      while ((stack.length > 0) && (top(stack) in ops) && (ops[top(stack)]>=ops[t])) {
+        let current = stack.pop();
+        result.push(current);
+      }
+      stack.push(t);
+    }
+  }
+  while (stack.length > 0) {
+    current = stack.pop();
+    if (current != ",") {
+      result.push(current);
+    }
+  }
+  console.log(`result is ${result}`);
+}
+
+function isNumber(t) {
+  return !isNaN(parseFloat(t)) && isFinite(t);
+}
+
+function isIdentifier(t) {
+  return t.startsWith("param.");
 }
 
 // ------------------------------------------------------------
